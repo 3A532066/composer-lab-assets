d1aa10f1354827022bdb181d982d3a9c53f31438
3A532066 <bcdling05@gmail.com>
2018-10-25 18:41:59 +0800

    初次提交 & DB setting

---
+++ bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+require  __DIR__.'/vendor/autoload.php';
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 33060);
+define('DB_DATABASE', 'blog_0804');
+define('DB_CHARSET', 'utf8');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+
+date_default_timezone_set('Asia/Taipei');
